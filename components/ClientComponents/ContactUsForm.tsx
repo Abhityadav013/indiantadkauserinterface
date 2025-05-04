abd4c89adf253525c063829111bb500e3a221120
Abhit Yadav <abhityadav013@gmail.com>
2025-05-04 21:14:10 +0530

    feat: add tailwind-merge dependency and clean up imports in ContactUsForm

--- components/ClientComponents/ContactUsForm.tsx
+++ components/ClientComponents/ContactUsForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { TextField, Button, Box } from "@mui/material";
 import toast from 'react-hot-toast';
 const ContactUsForm = () => {
