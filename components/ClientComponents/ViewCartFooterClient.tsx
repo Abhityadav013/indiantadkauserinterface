ab68e8ce46f8bf51d77d3e8c1a40f9c7d0ab637e
IndianTadka <indiantadka.dashboard@gmail.com>
2025-06-24 17:05:26 +0530

    feat: Define prop types for BasketSidebarClient and ViewCartFooterClient components

--- components/ClientComponents/ViewCartFooterClient.tsx
+++ components/ClientComponents/ViewCartFooterClient.tsx
@@ -1,6 +1,11 @@
 'use client';
+import { MenuItem } from '@/lib/types/menu_type';
 import ViewCartFooter from '../ViewCartFooter';
 
-export default function ViewCartFooterClient(props: any) {
+interface ViewCartFooterProps {
+    itmesCount: number;
+    menuItems: MenuItem[]
+}
+export default function ViewCartFooterClient(props: ViewCartFooterProps) {
   return <ViewCartFooter {...props} />;
 } 
